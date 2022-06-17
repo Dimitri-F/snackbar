1531aaba851c0e545a219dd31b015435301ca0eb
Dimitri-F <dimitri.fonsat@gmail.com>
2022-06-17 13:08:34 +0200

    finalized project

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function animSnackbar() {
     const snackbar = document.getElementById('snackbar')
-
-    snackbar.classList.toggle('show')
+        snackbar.classList.toggle('show') 
+        setTimeout(function() {
+            snackbar.classList.toggle('show')
+        }, 3000)
 }
